f3e6d41e50c46bc495b23540e78997776bf8c054
SupportVectorMcs <ronhe44@gmail.com>
2015-09-23 13:16:03 -0400

    update

---
+++ Buffer Lab/buflab-handout/example.d
@@ -0,0 +1,11 @@
+
+example.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	8d 6c 24 28          	lea    0x28(%esp),%ebp
+   4:	b8 50 73 84 3c       	mov    $0x3c847350,%eax
+   9:	68 4e 8c 04 08       	push   $0x8048c4e
+   e:	c3                   	ret    
---
+++ Buffer Lab/buflab-handout/example.s
@@ -0,0 +1,4 @@
+lea 0x28(%esp),%ebp
+mov $0x3c847350,%eax
+pushl $0x8048c4e
+ret
